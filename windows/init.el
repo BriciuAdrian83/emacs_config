91ab178b35799bbab1d5603ea1b84b4f16a4e595
ADRIAN NICOLAE BRICIU <60313520+BriciuAdrian83@users.noreply.github.com>
2026-09-07 09:16:09 +0300

    Enhance Emacs configuration with UI and completion packages
    Added various use-package configurations for UI enhancements and completion improvements.

--- windows/init.el
+++ windows/init.el
@@ -2,20 +2,40 @@
 (setq w32-use-visible-system-caret nil)
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file t)
 
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Show vertical options in the minibuffer
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode 1))
+
+;; Show more information on information in minibuffer
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode 1))
+
+;; Fix prefix complition in searching to match findings dinamically not respectiong a order
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-default nil))
+
+;; Persisting information about visited file on next open built into emacs (nil instead of t - true)
+(use-package savehist
+  :ensure nil
+  :config
+  (savehist-mode 1))
+
+;; Erase the selected text and insert new text
+(use-package delsel
+  :ensure nil
+  :config
+  (delete-selection-mode 1))
 
 
 ;; Global UI Zoom increased for Windows
